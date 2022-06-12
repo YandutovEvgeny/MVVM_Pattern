d04745bee40a80fed74e958efd22deb9b065443d
yandutka@mail.ru <yandutka@mail.ru>
2022-06-12 16:14:15 +0300

    commit

---
+++ TelephoneBook/AddViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TelephoneBook
+{
+    class AddViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void Notify(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        DataBaseModel recordModel;
+        Record record;
+
+        public AddViewModel()
+        {
+            recordModel = new DataBaseModel();
+            record = new Record();
+        }
+
+        public ICommand AddRecordButton
+        {
+            get
+            {
+                return new ButtonCommand(new Action(() =>
+                {
+                    if (recordModel.GetAllRecords().Count >= 1)
+                        record.Id = recordModel.GetAllRecords()[recordModel.GetAllRecords().Count - 1].Id;
+                    else
+                        record.Id = 0;
+                    record.Id += 1;
+                    recordModel.AddRecord(record);
+                    MessageBox.Show("Контакт добавлен!");
+                }));
+            }
+        }
+
+
+        public Record AddRecord
+        {
+            get { return record; }
+            set
+            {
+                record = value;
+                Notify("AddRecord");
+            }
+        }
+
+    }
+}
